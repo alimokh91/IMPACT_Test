f2b9909175826a2b89cacbb9d207d59ab078efc2
Guest <guest@gpucandoit.unibe.ch>
2019-05-22 10:52:22 +0200

    Addapted move an run shell to make it easier with new folder generation

--- prog/data/move.sh
+++ prog/data/move.sh
@@ -1,7 +1,6 @@
-mv *_large* result/
+mv *large* result/
 mv data_* result/
-cp *.txt result/
-rm turb_statxz*
-rm output.txt
+mv kf_result result/
+mv *.txt result/
+cp result/config.txt .
 touch output.txt
-mkdir data_01 data_02 data_03 data_04 data_05 data_06
