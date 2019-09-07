897e415521d061432ba96e4913c4f9d0045b0de2
Lukas Drescher <lukas.drescher@cscs.ch>
2019-09-07 17:53:12 +0200

    Fixed test launcher

--- test.sh
+++ test.sh
@@ -7,4 +7,3 @@ PYTHONPATH=python python test/test_sequential_mr_io_bidirectional.py
 PYTHONPATH=python python test/test_parallel_mr_io.py
 PYTHONPATH=python python test/test_parallel_mr_io_bidirectional.py
 PYTHONPATH=python python test/test_impact_input.py
-PYTHONPATH=python python test/test_impact_input_padding.py
