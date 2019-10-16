0dc3c70fa9f7d21c3bbcfab47b2c1794e4184f14
Andreas Fink <andreas.fink@cscs.ch>
2019-10-16 15:11:10 +0200

    use jinja template to produce xdmf files. Include also other fields from possible hpc-predict-io files

--- python/hdf5xdmf.py
+++ python/hdf5xdmf.py
@@ -1,13 +1,98 @@
 import h5py
+import jinja2
 import os
 import sys
+
+
+xdmfTemplate = """
+<Xdmf>
+  <Domain>
+    <Grid Name="GridTime" GridType="Collection" CollectionType="Temporal">
+    {% for curIdx in range(timeSlices.shape[0]) -%}
+      <Grid Name="Grid" GridType="Uniform">
+        <Time Value="{{ timeSlices[curIdx] }}" />
+        <Topology TopologyType="3DRectMesh" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }}" />
+        <Geometry Type="VXVYVZ">
+          <DataItem Name="X" Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ xDims }}">{{ hdf5FileName }}:/{{ group }}/x_coordinates</DataItem>
+          <DataItem Name="Y" Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ yDims }}">{{ hdf5FileName }}:/{{ group }}/y_coordinates</DataItem>
+          <DataItem Name="Z" Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ zDims }}">{{ hdf5FileName }}:/{{ group }}/z_coordinates</DataItem>
+        </Geometry>
+        {% for scalar_field in scalar_fields -%}
+        <Attribute AttributeType="Scalar" Name="{{ scalar_field }}" Center="Node">
+          <DataItem ItemType="HyperSlab" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }}" Format="XML">
+            <DataItem Dimensions="3 4" Format="XML">
+              0 0 0 {{ curIdx }} <!-- coordinate start -->
+              1 1 1 1 <!-- stride -->
+              {{ zDims }} {{ yDims }} {{ xDims }} 1 <!-- count -->
+            </DataItem>
+            <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }} {{ tDims }}">
+              {{ hdf5FileName }}:/{{ group }}/{{ scalar_field }}
+            </DataItem>
+          </DataItem>
+        </Attribute>
+        {% endfor -%}
+        {% for vector_field in vector_fields -%}
+        <Attribute AttributeType="Vector" Name="{{ vector_field }}" Center="Node">
+          <DataItem ItemType="HyperSlab" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }} 3" Format="XML">
+            <DataItem Dimensions="3 5" Format="XML">
+              0 0 0 {{ curIdx }} 0<!-- coordinate start -->
+              1 1 1 1 1 <!-- stride -->
+              {{ zDims }} {{ yDims }} {{ xDims }} 1 3 <!-- count -->
+            </DataItem>
+            <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }} {{ tDims }} 3">
+              {{ hdf5FileName }}:/{{ group }}/velocity_mean
+            </DataItem>
+          </DataItem>
+        </Attribute>
+        <Attribute AttributeType="Scalar" Name="{{ vector_field }}_magnitude" Center="Node">
+          <DataItem ItemType="Function" Function="SQRT($0*$0 + $1*$1 + $2*$2)" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }}">
+            <DataItem ItemType="HyperSlab" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }}" Format="XML">
+              <DataItem Dimensions="3 5" Format="XML">
+                0 0 0 {{ curIdx }} 0 <!-- coordinate start -->
+                1 1 1 1 1 <!-- stride -->
+                {{ zDims }} {{ yDims }} {{ xDims }} 1 1 <!-- count -->
+              </DataItem>
+              <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }} {{ tDims }} 3">
+                {{ hdf5FileName }}:/{{ group }}/velocity_mean
+              </DataItem>
+            </DataItem>
+            <DataItem ItemType="HyperSlab" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }}" Format="XML">
+              <DataItem Dimensions="3 5" Format="XML">
+                0 0 0 {{ curIdx }} 1 <!-- coordinate start -->
+                1 1 1 1 1 <!-- stride -->
+                {{ zDims }} {{ yDims }} {{ xDims }} 1 1 <!-- count -->
+              </DataItem>
+              <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }} {{ tDims }} 3">
+                {{ hdf5FileName }}:/{{ group }}/velocity_mean
+              </DataItem>
+            </DataItem>
+            <DataItem ItemType="HyperSlab" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }}" Format="XML">
+              <DataItem Dimensions="3 5" Format="XML">
+                0 0 0 {{ curIdx }} 2 <!-- coordinate start -->
+                1 1 1 1 1 <!-- stride -->
+                {{ zDims }} {{ yDims }} {{ xDims }} 1 1 <!-- count -->
+              </DataItem>
+              <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{{ zDims }} {{ yDims }} {{ xDims }} {{ tDims }} 3">
+                {{ hdf5FileName }}:/{{ group }}/velocity_mean
+              </DataItem>
+            </DataItem>
+          </DataItem>
+       </Attribute>
+       {% endfor -%}
+      </Grid>
+    {% endfor -%}
+    </Grid>
+  </Domain>
+</Xdmf>
+"""
 
 if len(sys.argv) < 2:
     print("Usage: {} hdf5-file".format(sys.argv[0]))
     exit(1)
 
 group = "flow-mri"
-necesary_fiels = ('x_coordinates', 'y_coordinates', 'z_coordinates', 't_coordinates', 'intensity', 'velocity_mean')
+necesary_fiels = ('x_coordinates', 'y_coordinates', 'z_coordinates', 't_coordinates')
+# 'intensity', 'velocity_mean', 'voxel_feature'
 hdf5FileName = sys.argv[1]
 
 
@@ -22,11 +107,19 @@ for field in necesary_fiels:
         print("Missing field {} in file {}".format(field, hdf5FileName))
         exit(1)
 
-for field in ('intensity', 'velocity_mean'):
-    for idx,coordinates in zip((0,1,2,3), ('z_coordinates', 'y_coordinates', 'x_coordinates', 't_coordinates')):
-        if hdf5file[field].shape[idx] != hdf5file[coordinates].shape[0]:
-            print("The field {} does not have the correct dimensions for {}".format(field, coordinates))
-            exit(1)
+for field in ('intensity', 'velocity_mean', 'voxel_feature', 'segmentation_prob'):
+    if field in hdf5file:
+        for idx,coordinates in zip((0,1,2,3), ('z_coordinates', 'y_coordinates', 'x_coordinates', 't_coordinates')):
+            if hdf5file[field].shape[idx] != hdf5file[coordinates].shape[0]:
+                print("The field {} does not have the correct dimensions for {}".format(field, coordinates))
+                exit(1)
+
+scalar_fields = []
+vector_fields = []
+if 'intensity' in hdf5file: scalar_fields.append('intensity')
+if 'segmentation_prob' in hdf5file: scalar_fields.append('segmentation_prob')
+if 'velocity_mean' in hdf5file: vector_fields.append('velocity_mean')
+if 'voxel_feature' in hdf5file: vector_fields.append('voxel_feature')
 
 xdmfFile = os.path.splitext(hdf5FileName)[0]+'.xdmf'
 xdmfFile = open(xdmfFile, 'w')
@@ -36,80 +129,7 @@ yDims = hdf5file['y_coordinates'].shape[0]
 xDims = hdf5file['x_coordinates'].shape[0]
 tDims = hdf5file['t_coordinates'].shape[0]
 
-xdmfFile.write("<Xdmf>\n")
-xdmfFile.write("  <Domain>\n")
-timeSlices = hdf5file['t_coordinates']
-xdmfFile.write('    <Grid Name="GridTime" GridType="Collection" CollectionType="Temporal">\n')
-for time,idx in zip(timeSlices, range(len(timeSlices))):
-    xdmfFile.write('      <Grid Name="Grid" GridType="Uniform">\n')
-    xdmfFile.write('        <Time Value="{}" />\n'.format(time))
-    xdmfFile.write('        <Topology TopologyType="3DRectMesh" Dimensions="{} {} {}" />\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('        <Geometry Type="VXVYVZ">\n')
-    xdmfFile.write('          <DataItem Name="X" Format="HDF" NumberType="Float" Precision="8" Dimensions="{}">{}:/{}/x_coordinates</DataItem>\n'.format(xDims, os.path.basename(hdf5FileName), group))
-    xdmfFile.write('          <DataItem Name="Y" Format="HDF" NumberType="Float" Precision="8" Dimensions="{}">{}:/{}/y_coordinates</DataItem>\n'.format(yDims, os.path.basename(hdf5FileName), group))
-    xdmfFile.write('          <DataItem Name="Z" Format="HDF" NumberType="Float" Precision="8" Dimensions="{}">{}:/{}/z_coordinates</DataItem>\n'.format(zDims, os.path.basename(hdf5FileName), group))
-    xdmfFile.write('        </Geometry>\n')
-    xdmfFile.write('        <Attribute AttributeType="Scalar" Name="Intensity" Center="Node">\n')
-    xdmfFile.write('          <DataItem ItemType="HyperSlab" Dimensions="{} {} {}" Format="XML">\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('            <DataItem Dimensions="3 4" Format="XML">\n')
-    xdmfFile.write('              0 0 0 {} <!-- coordinate start -->\n'.format(idx))
-    xdmfFile.write('              1 1 1 1 <!-- stride -->\n')
-    xdmfFile.write('              {} {} {} 1 <!-- count -->\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('            <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{} {} {} {}">\n'.format(zDims, yDims, xDims, tDims))
-    xdmfFile.write('              {}:/{}/intensity\n'.format(os.path.basename(hdf5FileName), group))
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('          </DataItem>\n')
-    xdmfFile.write('        </Attribute>\n')
-    xdmfFile.write('        <Attribute AttributeType="Vector" Name="Velocity" Center="Node">\n')
-    xdmfFile.write('          <DataItem ItemType="HyperSlab" Dimensions="{} {} {} 3" Format="XML">\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('            <DataItem Dimensions="3 5" Format="XML">\n')
-    xdmfFile.write('              0 0 0 {} 0<!-- coordinate start -->\n'.format(idx))
-    xdmfFile.write('              1 1 1 1 1 <!-- stride -->\n')
-    xdmfFile.write('              {} {} {} 1 3 <!-- count -->\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('            <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{} {} {} {} 3">\n'.format(zDims, yDims, xDims, tDims))
-    xdmfFile.write('              {}:/{}/velocity_mean\n'.format(os.path.basename(hdf5FileName), group))
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('          </DataItem>\n')
-    xdmfFile.write('        </Attribute>\n')
-    xdmfFile.write('        <Attribute AttributeType="Scalar" Name="VelocityMagnitude" Center="Node">\n')
-    xdmfFile.write('          <DataItem ItemType="Function" Function="SQRT($0*$0 + $1*$1 + $2*$2)" Dimensions="{} {} {}">\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('            <DataItem ItemType="HyperSlab" Dimensions="{} {} {}" Format="XML">\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('              <DataItem Dimensions="3 5" Format="XML">\n')
-    xdmfFile.write('                0 0 0 {} 0 <!-- coordinate start -->\n'.format(idx))
-    xdmfFile.write('                1 1 1 1 1 <!-- stride -->\n')
-    xdmfFile.write('                {} {} {} 1 1 <!-- count -->\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('              </DataItem>\n')
-    xdmfFile.write('              <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{} {} {} {} 3">\n'.format(zDims, yDims, xDims, tDims))
-    xdmfFile.write('                {}:/{}/velocity_mean\n'.format(os.path.basename(hdf5FileName), group))
-    xdmfFile.write('              </DataItem>\n')
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('            <DataItem ItemType="HyperSlab" Dimensions="{} {} {}" Format="XML">\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('              <DataItem Dimensions="3 5" Format="XML">\n')
-    xdmfFile.write('                0 0 0 {} 1 <!-- coordinate start -->\n'.format(idx))
-    xdmfFile.write('                1 1 1 1 1 <!-- stride -->\n')
-    xdmfFile.write('                {} {} {} 1 1 <!-- count -->\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('              </DataItem>\n')
-    xdmfFile.write('              <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{} {} {} {} 3">\n'.format(zDims, yDims, xDims, tDims))
-    xdmfFile.write('                {}:/{}/velocity_mean\n'.format(os.path.basename(hdf5FileName), group))
-    xdmfFile.write('              </DataItem>\n')
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('            <DataItem ItemType="HyperSlab" Dimensions="{} {} {}" Format="XML">\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('              <DataItem Dimensions="3 5" Format="XML">\n')
-    xdmfFile.write('                0 0 0 {} 2 <!-- coordinate start -->\n'.format(idx))
-    xdmfFile.write('                1 1 1 1 1 <!-- stride -->\n')
-    xdmfFile.write('                {} {} {} 1 1 <!-- count -->\n'.format(zDims, yDims, xDims))
-    xdmfFile.write('              </DataItem>\n')
-    xdmfFile.write('              <DataItem Format="HDF" NumberType="Float" Precision="8" Dimensions="{} {} {} {} 3">\n'.format(zDims, yDims, xDims, tDims))
-    xdmfFile.write('                {}:/{}/velocity_mean\n'.format(os.path.basename(hdf5FileName), group))
-    xdmfFile.write('              </DataItem>\n')
-    xdmfFile.write('            </DataItem>\n')
-    xdmfFile.write('          </DataItem>\n')
-    xdmfFile.write('       </Attribute>\n')
-    xdmfFile.write('      </Grid>\n')
-xdmfFile.write('    </Grid>\n')
-xdmfFile.write('  </Domain>\n')
-xdmfFile.write('</Xdmf>\n')
-
+xdmfStr = jinja2.Template(xdmfTemplate).render(dict(xDims=xDims, yDims=yDims, zDims=zDims, tDims=tDims, scalar_fields=scalar_fields,
+    vector_fields=vector_fields, hdf5FileName=os.path.basename(hdf5FileName), group=group, timeSlices=hdf5file['t_coordinates']))
+xdmfFile.write(xdmfStr)
 
